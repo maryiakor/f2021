92a501f6c9b9ccd66e88fc6a6980a7700323b4ca
maryiakor <novash.mv@gmail.com>
2022-07-19 16:01:34 +0300

    Added ButtonProps

---
+++ src/components/ButtonsProps/ButtonProps.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ButtonProps.css";
+
+const ButtonProps = () => {
+  return <button className="my-button-props">Click1</button>;
+};
+
+export default ButtonProps;
