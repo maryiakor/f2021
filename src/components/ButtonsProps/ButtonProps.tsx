7ef37c733b0b7d08147f1930f9e989d6a28847cf
Mar <novash.mv@gmail.com>
2022-07-20 21:48:47 +0300

    Add hook 1

--- src/components/ButtonsProps/ButtonProps.tsx
+++ src/components/ButtonsProps/ButtonProps.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {FC} from "react";
 import "./ButtonProps.css";
 
-const ButtonProps = () => {
-  return <button className="my-button-props">Click1</button>;
+const ButtonProps = (
+  {
+    color = "blue",
+     disabled,
+      children,
+      onClick,
+    }:
+     {
+       // если вопр знак то, не обязательное свлйство
+       color?: string,
+       disabled?: boolean,
+       children: React.ReactNode,
+       onClick: () => void;
+      }) => {
+  return <button onClick={onClick} disabled = {disabled} style={{color:color}}>{children}</button>;
 };
 
 export default ButtonProps;
