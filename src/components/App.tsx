0d30e3daa089d052d058686277ed2af20cfac59a
Chigileychik Andrey <chigileychik_a@itstep.lan>
2022-08-15 21:53:47 +0300

    add Users component, feature delete and search

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Users from "./Users/Users";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
       </header>
       <h1 className="mt-5">My React App</h1>
+        <Users/>
     </div>
   );
 }
