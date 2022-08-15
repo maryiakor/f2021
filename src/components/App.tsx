da5acfdab2413e2bbc22a69f73af4de56d511cd3
Chigileychik Andrey <chigileychik_a@itstep.lan>
2022-08-15 19:33:53 +0300

    inplement bootstrap

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import Button from "./Button/Button";
-import Counter from "./Counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <h1>My React App</h1>
-      <Button/>
-      <Counter/>
+      <h1 className="mt-5">My React App</h1>
     </div>
   );
 }
